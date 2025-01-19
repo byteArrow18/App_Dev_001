6c4ac16e0363a0abc5ade5fac71faaf771f8a68f
byteArrow18 <bytearrow18@gmail.com>
2025-01-19 10:46:07 +0530

    created widget for add task button

--- lib/task/presentation/task.dart
+++ lib/task/presentation/task.dart
@@ -23,65 +23,76 @@ class _TaskePageState extends State<TaskPage> {
         body: Padding(
           padding: EdgeInsets.all(20),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-          ),
-          onPressed: () => showModalBottomSheet(
-            context: context,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20), // Rounded top-left corner
-                topRight: Radius.circular(20), // Rounded top-right corner
-              ),
-            ),
-            clipBehavior: Clip.hardEdge,
-            builder: (BuildContext context) => Container(
-              height: 150,
-              // color: Color.fromARGB(255, 238, 241, 239),
-              color: const Color.fromARGB(255, 237, 227, 255),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 350,
-                      height: 70,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          fillColor: Colors.white,
-                          filled: true,
-                          hintText: 'Enter task',
-                          // contentPadding: EdgeInsets.symmetric(vertical: 15),
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 20,),
-                    FloatingActionButton(
-                      onPressed: () => print('pressed'),
-                      elevation: 0,
-                      backgroundColor: Color.fromARGB(255, 178, 221, 192),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(500),
-                      ),
-                      child: Icon(
-                        Icons.send_outlined,
-                        color: Color.fromARGB(255, 19, 106, 48),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ),
-        ),
+        floatingActionButton: addTaskButton(),
       ),
     );
     // TODO: implement build
     // throw UnimplementedError();
+  }
+}
+
+class addTaskButton extends StatelessWidget {
+  const addTaskButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(
+        Icons.add,
+      ),
+      onPressed: () => showModalBottomSheet(
+        context: context,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20), // Rounded top-left corner
+            topRight: Radius.circular(20), // Rounded top-right corner
+          ),
+        ),
+        clipBehavior: Clip.hardEdge,
+        builder: (BuildContext context) => Container(
+          height: 150,
+          // color: Color.fromARGB(255, 238, 241, 239),
+          color: const Color.fromARGB(255, 237, 227, 255),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 5),
+                  width: 350,
+                  height: 70,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Enter task',
+                      // contentPadding: EdgeInsets.symmetric(vertical: 15),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20,),
+                FloatingActionButton(
+                  onPressed: () => print('pressed'),
+                  elevation: 0,
+                  backgroundColor: Color.fromARGB(255, 178, 221, 192),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(500),
+                  ),
+                  child: Icon(
+                    Icons.send_outlined,
+                    color: Color.fromARGB(255, 19, 106, 48),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ),
+    );
   }
 }
