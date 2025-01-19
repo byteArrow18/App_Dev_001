749e84fc26c6150ec9119570a1eaeefe0177b966
byteArrow18 <bytearrow18@gmail.com>
2025-01-19 10:47:52 +0530

    code split

--- lib/task/presentation/task.dart
+++ lib/task/presentation/task.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_final_fields
 
+import 'package:app_dev_001/task/presentation/addTaskButton.dart';
 import 'package:flutter/material.dart';
 
 
@@ -28,71 +29,5 @@ class _TaskePageState extends State<TaskPage> {
     );
     // TODO: implement build
     // throw UnimplementedError();
-  }
-}
-
-class addTaskButton extends StatelessWidget {
-  const addTaskButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      child: Icon(
-        Icons.add,
-      ),
-      onPressed: () => showModalBottomSheet(
-        context: context,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20), // Rounded top-left corner
-            topRight: Radius.circular(20), // Rounded top-right corner
-          ),
-        ),
-        clipBehavior: Clip.hardEdge,
-        builder: (BuildContext context) => Container(
-          height: 150,
-          // color: Color.fromARGB(255, 238, 241, 239),
-          color: const Color.fromARGB(255, 237, 227, 255),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 5),
-                  width: 350,
-                  height: 70,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      fillColor: Colors.white,
-                      filled: true,
-                      hintText: 'Enter task',
-                      // contentPadding: EdgeInsets.symmetric(vertical: 15),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 20,),
-                FloatingActionButton(
-                  onPressed: () => print('pressed'),
-                  elevation: 0,
-                  backgroundColor: Color.fromARGB(255, 178, 221, 192),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(500),
-                  ),
-                  child: Icon(
-                    Icons.send_outlined,
-                    color: Color.fromARGB(255, 19, 106, 48),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )
-      ),
-    );
   }
 }
